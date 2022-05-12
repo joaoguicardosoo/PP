8fb5097b8521c90643b0caf5c5e2231a1c976bd7
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-05-12 20:22:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -106,5 +106,126 @@ return 0;
 }
 */
 
-// Exercicio 10
+/* Exercicio 10
+int main (){
+    float km, ms;
+    printf("Qual a velocidade em km/h? \n");
+    scanf("%f", &km);
+    ms = km/3.6;
+    printf("Sua velocidade em m/s eh %.1f", ms);
+return 0;
+}
+*/
 
+/* Exercicio 11
+int main (){
+    float ms, km;
+    printf("Qual a velocidade em m/s? \n");
+    scanf("%f", &ms);
+    km = ms*3.6;
+    printf("Sua velocidade em km/h eh %.1f", km);
+return 0;
+}
+*/
+
+/* Exercicio 12
+int main (){
+    float milhas, km;
+    printf("Qual a distancia em milhas? \n");
+    scanf("%f", &milhas);
+    km = 1.61*milhas;
+    printf("Sua distancia em km eh %.1f", km);
+return 0;
+}
+*/
+
+/* Exercicio 13
+int main (){
+    float km, milhas;
+    printf("Qual a distancia em km? \n");
+    scanf("%f", &km);
+    milhas = km/1.61;
+    printf("Sua distancia em milhas eh %.1f", milhas);
+return 0;
+}
+*/
+
+/* Exercicio 14
+int main(){
+    float anggraus, angradianos;
+    printf("Digite um angulo em graus: \n");
+    scanf("%f", &anggraus);
+    angradianos = anggraus*3.141592/180;
+    printf("Seu angulo em radianos eh %.4f", angradianos);
+return 0;
+}
+*/
+
+/* Exercicio 15
+int main(){
+    float angradianos, anggraus;
+    printf("Digite um angulo em radianos: \n");
+    scanf("%f", &angradianos);
+    anggraus = angradianos*180/3.141592;
+    printf("Seu angulo em graus eh %.4fC.", anggraus);
+return 0;
+}
+*/
+
+/* Exercicio 16
+int main (){
+    float polegada, centimetro;
+    printf("Digite um valor em polegadas: \n");
+    scanf("%f", &polegada);
+    centimetro=polegada*2.54;
+    printf("Seu valor em centimetros eh: %.2f", centimetro);
+return 0;
+}
+*/
+
+/* Exercicio 17
+int main (){
+    float centimetro, polegada;
+    printf("Digite um valor em centimetros: \n");
+    scanf("%f", &centimetro);
+    polegada=centimetro/2.54;
+    printf("Seu valor em polegadas eh: %.2f", polegada);
+return 0;
+}
+*/
+
+/* Exercicio 18
+int main (){
+    float metros, litros;
+    printf("Digite o valor do volume em metros cubicos: \n");
+    scanf("%f", &metros);
+    litros = 1000*metros;
+    printf("O valor do seu volume em litros eh: %.1f", litros);
+
+return 0;
+}
+*/
+
+/* Exercicio 19
+int main (){
+    float litros, metros;
+    printf("Digite o valor do volume em litros: \n");
+    scanf("%f", &litros);
+    metros = litros/1000;
+    printf("O valor do seu volume em metros cubicos eh: %.1f", metros);
+
+return 0;
+}
+*/
+
+// Exercicio 20
+int main (){
+    float kg, libras;
+    printf("Digite o valor de massa em kg: \n");
+    scanf("%f", &kg);
+    libras = kg/0.45;
+    printf("O valor de massa em libras eh: %.1f", libras);
+
+return 0;
+}
+//
