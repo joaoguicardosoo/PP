aa8d9a9d8794e4a3ce74390e83b0fef5b182f7cf
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-05-12 19:51:35 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+ /* Exercício 1
+int main(){
+    int x;
+    printf("Digite um numero inteiro: \n");
+    scanf("%d", &x);
+
+    printf("O numero inteiro e: %d", x);
+
+return 0;
+}
+*/
+
+/* Exercício 2
+int main(){
+    float x;
+    printf("Digite um numero real: \n");
+    scanf("%f", &x);
+
+    printf("O numero real e: %f", x);
+
+return 0;
+}
+*/
+
+/* Exercício 3
+int main (){
+    int x,y,z,soma;
+    printf("Digite tres numeros inteiros separados por espaco: \n");
+    scanf("%d %d %d", &x, &y, &z);
+    soma = x+y+z;
+    printf("A soma dos tres numeros eh %d.", soma);
+return 0;
+}
+*/
+
+/* Exercicio 4
+int main (){
+    float x, quadrado;
+    printf("Digite um numero real: \n");
+    scanf("%f", &x);
+    quadrado=x*x;
+    printf("O quadrado do numero digitado eh: %f", quadrado);
+return 0;
+}
+*/
+
+
+/* Exercicio 5
+int main (){
+    float x, quintaparte;
+    printf("Digite um numero real: \n");
+    scanf("%f", &x);
+    quintaparte=x/5;
+    printf("A quinta parte deste numero eh %f", quintaparte);
+return 0;
+}
+*/
+
+/* Exercicio 6
+int main () {
+    float celsius, fahrenheit;
+    printf("Digite uma temperatura em graus Celsius: \n");
+    scanf("%f", &celsius);
+    fahrenheit = celsius*(9.0/5.0)+32;
+    printf("A temperatura em Celsius convertida em fahrenheit eh: %.1fF", fahrenheit);
+return 0;
+}
+*/
+
+/* Exercicio 7
+int main (){
+    float celsius, fahrenheit;
+    printf("Digite uma temperatura em graus Fahrenheit: \n");
+    scanf("%f", &fahrenheit);
+    celsius = 5.0*(fahrenheit-32.0)/9.0;
+    printf("A temperatura em Fahrenheit convertida em Celsius eh: %.1fC", celsius);
+return 0;
+}
+*/
+
+/* Exercicio 8
+int main (){
+    float kelvin, celsius;
+    printf("Digite uma temperatura em graus Kelvin: \n");
+    scanf("%f", &kelvin);
+    celsius = kelvin-273,15;
+    printf("A temperatura em Kelvin convertida em Celsius eh: %.1fC", celsius);
+
+return 0;
+}
+*/
+
+/* Exercicio 9
+int main (){
+    float celsius, kelvin;
+    printf("Digite uma temperatura em graus Celsius: \n");
+    scanf("%f", &kelvin);
+    kelvin = celsius+273,15;
+    printf("A temperatura em Celsius convertida em Kelvin eh: %.1fC", kelvin);
+
+return 0;
+}
+*/
+
+// Exercicio 10
+
