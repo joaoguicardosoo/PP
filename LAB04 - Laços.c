cae710fdb8ed750206d7af5cd93f8e73433cea07
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-05-26 19:36:43 -0300

    Update LAB04 - Laços.c

--- LAB04 - Laços.c
+++ LAB04 - Laços.c
@@ -4,13 +4,34 @@
 
 // Programacao Procedimental - LAB04 Laços
 
-// Exercicio 1
+/* Exercicio 1
 
 int main (){
     int contador, resultado;
     for (contador = 1; contador <= 5; contador++){
         resultado = 3*contador;
         printf("O multiplo %d de 3 eh %d. \n", contador, resultado);;
+    }
+
+return 0;
+}
+*/
+
+// Exercicio 2
+int main (){
+    int contadorfor, contadorwhile = 1, contadordowhile = 1;
+    printf("Usando for: \n");
+
+    if (contadorfor <= 100){
+    for (contadorfor = 0; contadorfor <= 100; contadorfor++){
+        printf("%d ", contadorfor);
+        }
+    }
+
+    printf("Usando while: \n");
+    while (contadorwhile <= 100){
+        printf("%d ", contadordowhile);
+        contadordowhile ++;
     }
 
 return 0;
