152f9447b74cbfdb2358903b46725a2705f60c30
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-05-26 19:23:49 -0300

    Update LAB04 - Laços.c

--- LAB04 - Laços.c
+++ LAB04 - Laços.c
@@ -2,3 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Programacao Procedimental - LAB04 Laços
+
+// Exercicio 1
+
+int main (){
+    int contador, multiplos, resultado;
+    for (contador = 1; contador <= 5; contador++){
+        resultado = 3*contador;
+        printf("O multiplo %d de 3 eh %d \n", contador, resultado);
+        contador++;
+    }
+
+return 0;
+}
