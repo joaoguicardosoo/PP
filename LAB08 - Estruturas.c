a28c73959e268fc9cf7cce248cf0634ae04619b7
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-06-23 19:30:28 -0300

    Update LAB08 - Estruturas.c

--- LAB08 - Estruturas.c
+++ LAB08 - Estruturas.c
@@ -4,12 +4,24 @@
 
 
 // Exercicio 1
+    struct horario{
+        int hora;
+        int minutos;
+        int segundos;
+    };
+    struct data{
+        int dia;
+        int mes;
+        int ano;
+    };
+    struct compromisso{
+        struct data;
+        struct horario;
+        char texto;
+    };
+
 void main (){
-
-
-
-
-
+    struct compromisso cp1 =
 
 
 }
