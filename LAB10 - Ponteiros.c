304b26e38350ba0e3e5b3a8a156644d8fefe2489
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-07-14 20:37:30 -0300

    Update LAB10 - Ponteiros.c

--- LAB10 - Ponteiros.c
+++ LAB10 - Ponteiros.c
@@ -4,3 +4,42 @@
 
 
 // Exercicio 1
+
+int main (){
+    int x, *a;
+    float y, *b;
+    char z, *c;
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &x);
+    printf("Digite um valor float: ");
+    scanf("%f", &y);
+    printf("Digite uma letra: ");
+    fflush(stdin);
+    scanf("%c", &z);
+
+
+    printf("Inteiro = %d \n", x);
+    printf("Float = %f \n", y);
+    printf("Char = %c \n", z);
+
+
+    a = &x;
+    b = &y;
+    c = &z;
+
+    *a = 5;
+    *b = 2.3;
+    *c = 't';
+
+    printf("Inteiro ponteiro: %d \n", *a);
+    printf("Float ponteiro: %f \n", *b);
+    printf("Char ponteiro: %c \n", *c);
+
+
+
+return 0;
+}
+//
+
+// Exercício 2
