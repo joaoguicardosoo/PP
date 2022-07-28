8a45f0b966ec2a0b601db67bc3f80841710746a2
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-07-28 19:42:09 -0300

    Update LAB11 - Memória.c

--- LAB11 - Memória.c
+++ LAB11 - Memória.c
@@ -4,4 +4,19 @@
 
 
 
-/* Exercicio 1
+// Exercicio 1
+int main (){)
+    int tam, i;
+    int *x;
+
+    printf("Qual sera o tamanho do array? ");
+    scanf("%d", &tam);
+
+    x = (int *) malloc(tam*(sizeof(int)));
+
+    for (i=0; i<tam; i++){
+        printf("Insira posicao %d: \n", i);
+        scanf("%d", *x[i]);
+    }
+
+    
